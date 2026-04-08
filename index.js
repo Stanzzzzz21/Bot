0f2d87f06d8a8d0cd9cf272ecb8845bb068f633f
Stanzzzzz21 <stanislavstoykov3@gmail.com>
2026-04-08 13:49:41 +0100

    Integrate Express server for bot status
    Added Express server to respond with a status message.

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const {
   Routes,
   SlashCommandBuilder,
   PermissionsBitField
+  const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot is running'));
+app.listen(3000, () => console.log('Server running'));
 } = require('discord.js');
 
 const fs = require('fs');
